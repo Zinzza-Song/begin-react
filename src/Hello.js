861099daa028adfb60bd8115a53f288c41c6baa3
Han-Bin Song <thdgksq@naver.com>
2025-02-15 14:18:35 +0900

    input 상태 관리(useState 이용)

--- src/Hello.js
+++ src/Hello.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Hello({ color, name, isSpecial }) {
+function Hello({ color, name = "이름없음", isSpecial }) {
   return (
     <div style={{ color }}>
       {isSpecial && <b>*</b>}
@@ -8,9 +8,5 @@ function Hello({ color, name, isSpecial }) {
     </div>
   );
 }
-
-Hello.defaultProps = {
-  name: "이름없음",
-};
 
 export default Hello;
