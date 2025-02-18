b5efa3aa0ee024dc0097c00a2067ba7b62f244a1
Han-Bin Song <thdgksq@naver.com>
2025-02-18 13:38:03 +0900

    useEffect 사용

--- src/UserList.js
+++ src/UserList.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function User({ user, onRemove, onToogle }) {
   const { username, email, id, active } = user;
+
+  // useEffect(() => {
+  //   console.log("컴포넌트가 화면에 나타남");
+  //   // 마운트 시 작동 마운트? -> 화면이 나타남
+  //   // props -> state
+  //   // REST API
+  //   // D3 Video.js
+  //   // setInterval, setTimeout
+  //   return () => {
+  //     // 언마운트시 작동
+  //     // clearInterval, clearTimeout
+  //     // 라이브러리 인스턴스 제거
+  //     console.log("컴포넌트가 화면에서 사라짐");
+  //   };
+  // }, []);
+
+  useEffect(() => {
+    console.log("user 값이 설정됨");
+    console.log(user);
+
+    return () => {
+      console.log("user 값이 바뀌기 전");
+      console.log(user);
+    };
+  }, [user]);
 
   return (
     <div>
