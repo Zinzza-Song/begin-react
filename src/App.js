9135ff58a111ba5d11771ab744dec753b815c903
Han-Bin Song <thdgksq@naver.com>
2025-02-19 14:13:29 +0900

    useState -> useReduce로 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo, useCallback } from "react";
+import React, { useRef, useReducer, useMemo, useCallback } from "react";
 import CreateUser from "./CreateUser";
 import UserList from "./UserList";
 
@@ -8,28 +8,13 @@ function countActiveUsers(users) {
   return users.filter((user) => user.active).length;
 }
 
-function App() {
-  const [inputs, setInputs] = useState({
+const initialState = {
+  inputs: {
     username: "",
     email: "",
     active: true,
-  });
-
-  const { username, email } = inputs;
-
-  //useCallback를 활용하여 함수 재활용
-  const onChange = useCallback(
-    (e) => {
-      const { name, value } = e.target;
-      setInputs({
-        ...inputs,
-        [name]: value,
-      });
-    },
-    [inputs]
-  );
-
-  const [users, setUsers] = useState([
+  },
+  users: [
     {
       id: 1,
       username: "zinzza",
@@ -48,42 +33,84 @@ function App() {
       email: "song@naver.com",
       active: false,
     },
-  ]);
+  ],
+};
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "CHANGE_INPUT":
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value,
+        },
+      };
+    case "CREATE_USER":
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user),
+      };
+    case "TOOGLE_USER":
+      return {
+        ...state,
+        users: state.users.map((user) =>
+          user.id === action.id ? { ...user, active: !user.active } : user
+        ),
+      };
+    case "REMOVE_USER":
+      return {
+        ...state,
+        users: state.users.filter((user) => user.id !== action.id),
+      };
+    default:
+      throw new Error("Unhandled action");
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const nextId = useRef(4);
+  const { users } = state;
+  const { username, email } = state.inputs;
 
-  const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      ...inputs,
-    };
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
 
-    // setUsers([...users, user]); 추가 방법 1
-    setUsers((users) => users.concat(user)); // 추가 방법 2
-
-    setInputs({
-      username: "",
-      email: "",
+    dispatch({
+      type: "CHANGE_INPUT",
+      name,
+      value,
     });
-
-    nextId.current += 1;
-  }, [inputs]);
-
-  const onRemve = useCallback((id) => {
-    setUsers((users) => users.filter((user) => user.id !== id));
   }, []);
 
+  const onCreate = useCallback(() => {
+    dispatch({
+      type: "CREATE_USER",
+      user: {
+        id: nextId.current,
+        username,
+        email,
+      },
+    });
+    nextId.current += 1;
+  }, [username, email]);
+
   const onToogle = useCallback((id) => {
-    setUsers((users) =>
-      users.map((user) =>
-        user.id === id ? { ...user, active: !user.active } : user
-      )
-    );
+    dispatch({
+      type: "TOOGLE_USER",
+      id,
+    });
+  }, []);
+
+  const onRemove = useCallback((id) => {
+    dispatch({
+      type: "REMOVE_USER",
+      id,
+    });
   }, []);
 
   const count = useMemo(() => countActiveUsers(users), [users]);
-  // useMemo를 사용안하면 화면이 리렌더링 될 때마다 계속 연산이됨
-  // useMemo를 사용하면 해당 값이 변할 때만 연산(기존의 값을 재사용 하여 최적화)
 
   return (
     <>
@@ -93,7 +120,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemve} onToogle={onToogle} />
+      <UserList users={users} onToogle={onToogle} onRemove={onRemove} />
       <div>활성 사용자 수: {count}</div>
     </>
   );
