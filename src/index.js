07adef9c502c48829fc84aa5fc5c37a241040aba
Han-Bin Song <thdgksq@naver.com>
2025-02-22 13:53:02 +0900

    Context API를 활용한 전역 값 관리

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 // import Counter from "./Counter";
 import reportWebVitals from "./reportWebVitals";
+import ContextSample from "./ContextSample";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ContextSample />
   </React.StrictMode>
 );
 
